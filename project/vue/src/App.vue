8c295ae62f1fc09644b272c0aa620c22b4cb9b2d
morn sovathana <mornsovathana@gmail.com>
2025-03-30 21:17:47 +0700

    ref

--- project/vue/src/App.vue
+++ project/vue/src/App.vue
@@ -1,26 +1,52 @@
-<script>
-  export default{
-    data(){
-      return {
-        name: 'Jonh',
-        status: true,
-        status1: 'active',
-        tasks: ['test1','test2'],
-        link: 'https://chatgpt.com/c/67ea0af1-3cdc-800f-a52d-d4e8233f5d90',
-      }
-    },
-    methods: {
-      ToggleStatus(){
-        if(this.status1 === 'active' ){
-          this.status1 = 'pedding';
-        }else if(this.status1 === 'pedding'){
-          this.status1 = 'inactive';
+<script setup>
+  // export default{
+  //   data(){
+  //     return {
+  //       name: 'Jonh',
+  //       status: true,
+  //       status1: 'active',
+  //       tasks: ['test1','test2'],
+  //       link: 'https://chatgpt.com/c/67ea0af1-3cdc-800f-a52d-d4e8233f5d90',
+  //     }
+  //   },
+  //   methods: {
+  //     ToggleStatus(){
+  //       if(this.status1 === 'active' ){
+  //         this.status1 = 'pedding';
+  //       }else if(this.status1 === 'pedding'){
+  //         this.status1 = 'inactive';
+  //       }else{
+  //         this.status1 = 'active';
+  //       }
+  //     },
+  //   },
+  // }
+  import { ref } from 'vue';
+
+      const name = ref('vathana');
+      const status1 = ref('active');
+      const tasks = ref(['task1','task2','task3']);
+
+      const newTask = ref('adfsdf');
+      const ToggleStatus = () =>{
+        if(status1.value === 'active' ){
+         status1.value = 'pedding';
+        }else if(status1.value === 'pedding'){
+          status1.value = 'inactive';
         }else{
-          this.status1 = 'active';
+          status1.value = 'active';
         }
-      },
-    },
-  }
+      }
+
+      const addTask = () =>{
+        if(newTask.value.trim() !== ''){
+          tasks.value.push(newTask.value);
+          newTask.value = '';
+        }
+      }
+      const deleteTask = (index) =>{
+        tasks.value.splice(index,1);
+      }
 </script>
 <template>
   <h1>{{ name }}</h1>
@@ -28,10 +54,17 @@
   <p v-else>not</p>
   <p v-if="status1 === 'active'">active</p>
   <p v-if="status1 === 'pedding'">pedding</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id = "newTask" name = "newTask" v-model = "newTask">
+    <button type = "submit">Submit</button>
+  </form>
   <h1>Task:</h1>
   <ul>
-    <li v-for = "task in tasks" :key="task">
-      {{ task }}
+    <li v-for = "(task,index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click = "deleteTask(index)">x</button>
     </li>
   </ul>
   <a :href="link">google</a>
